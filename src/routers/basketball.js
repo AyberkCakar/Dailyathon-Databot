77aada9da1205f3431826160dd1d6d409c4132bc
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-10 21:53:10 +0300

    feat: added basketball bot

--- src/routers/basketball.js
+++ src/routers/basketball.js
@@ -15,21 +15,20 @@ async function getLeague() {
 }
 
 /*
-async function setLeagueData(SequenceNo,TeamName,TeamLogoUrl,O,G,B,M,A,Y,AV,P,LeagueID) {
+async function setLeagueData(SequenceNo,TeamName,TeamLogoUrl,O,G,M,A,Y,AV,P,LeagueID) {
     await axios.post('https://dailyathon.herokuapp.com/league-standings', {
         SequenceNo: SequenceNo,
         TeamName: TeamName,
         TeamLogoUrl: TeamLogoUrl,
         O: O,
         G: G,
-        B: B,
         M: M,
         A: A,
         Y: Y,
         AV: AV,
         P: P,
         LeagueID: LeagueID,
-        LeagueTableName: 'tblFootball'
+        LeagueTableName: 'tblBasketball'
     })
         .then(function (response) {
             console.log(response.status);
@@ -40,21 +39,20 @@ async function setLeagueData(SequenceNo,TeamName,TeamLogoUrl,O,G,B,M,A,Y,AV,P,Le
 }
 */
 
-async function updateLeagueData(SequenceNo,TeamName,TeamLogoUrl,O,G,B,M,A,Y,AV,P,LeagueID) {
+async function updateLeagueData(SequenceNo,TeamName,TeamLogoUrl,O,G,M,A,Y,AV,P,LeagueID) {
     await axios.put('https://dailyathon.herokuapp.com/league-standings', {
             SequenceNo: SequenceNo,
             TeamName: TeamName,
             TeamLogoUrl: TeamLogoUrl,
             O: O,
             G: G,
-            B: B,
             M: M,
             A: A,
             Y: Y,
             AV: AV,
             P: P,
             LeagueID: LeagueID,
-            LeagueTableName: 'tblFootball'
+            LeagueTableName: 'tblBasketball'
     })
         .then(function (response) {
             console.log(response.status);
@@ -66,23 +64,22 @@ async function updateLeagueData(SequenceNo,TeamName,TeamLogoUrl,O,G,B,M,A,Y,AV,P
 
 function LeagueData(data) {
     request(data.LeagueUrl, function (error, response, html) {
-        if(data.SportID == 1)
+        if(data.SportID == 2)
         {
             if (! error && response.statusCode == 200 ) {
                 const $ = cheerio.load(html);
                 $ ('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status').each(function(i, element){
                     const SequenceNo = $ (this).find('span.p0c-competition-tables__rank').text();
                     const TeamName = $ (this).find('img.p0c-competition-tables__crest').attr('src');
-                    const TeamLogoUrl = $ (this).find('span.p0c-competition-tables__team-name.p0c-competition-tables__team-name--full').text();
-                    const O = $ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(3).text();
-                    const G =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(5).text();
-                    const B =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(6).text();
-                    const M =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(7).text();
-                    const A =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(8).text();
-                    const Y =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(9).text();
-                    const AV =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(10).text();
-                    const P =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(11).text();
-                    updateLeagueData(SequenceNo,TeamLogoUrl,TeamName,O,G,B,M,A,Y,AV,P,data.LeagueID)
+                    const TeamLogoUrl = $ (this).find('span.p0c-competition-tables__team-name').first().text();
+                    const O = $ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(2).text();
+                    const G =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(3).text();
+                    const M =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(4).text();
+                    const A =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(5).text();
+                    const Y =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(6).text();
+                    const AV =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(7).text();
+                    const P =$ (this).find('tr.p0c-competition-tables__row.p0c-competition-tables__row--rank-status>td').eq(8).text();
+                    updateLeagueData(SequenceNo,TeamLogoUrl,TeamName,O,G,M,A,Y,AV,P,data.LeagueID)
                 });
             }
             else
@@ -92,11 +89,12 @@ function LeagueData(data) {
         }
         else
         {
-            console.log("Sport name is not football !")
+            console.log("Sport name is not basketball !")
         }
 
     });
 }
+
 
 const hours = (1000 * 60 * 60 * 4);
 // setInterval(intervalFunc, hours);
