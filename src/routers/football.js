1b5c4c1633004713b2ac5fee3a72dc347e0a1546
AyberkCakar <ayberkcakar35@gmail.com>
2020-11-15 13:53:57 +0300

    fix: fix football log servicee

--- src/routers/football.js
+++ src/routers/football.js
@@ -14,7 +14,7 @@ async function getLeague() {
             }
         )
     } catch (error) {
-        console.error(error);
+        log.createLog('Football Bot','','getLeague Error',error.data.message);
     }
 }
 
@@ -24,12 +24,10 @@ async function setLeagueData(legueUrl,leagueName) {
         query:sqlQuery
     })
         .then(function (response) {
-            log.createLog('Football Bot',legueUrl,leagueName+ ' League information added successfully.',' ');
-            console.log(response.status);
+            log.createLog('Football Bot',legueUrl,leagueName+ ' information added successfully.',response.data.message);
         })
         .catch(function (error) {
-            log.createLog('Football Bot',legueUrl,leagueName+ ' League information added failed.',error.message);
-            console.log(error);
+            log.createLog('Football Bot',legueUrl,leagueName+ ' information added failed.',error.data.message);
         });
 }
 
@@ -47,12 +45,10 @@ async function updateLeagueData(legueUrl,leagueName) {
         query:sqlQuery
     })
     .then(function (response) {
-        log.createLog('Football Bot',legueUrl,leagueName+ ' League information added successfully.',' ');
-        console.log(response.status);
+        log.createLog('Football Bot',legueUrl,leagueName+ ' League information updated successfully.',response.data.message);
     })
     .catch(function (error) {
-        log.createLog('Football Bot',legueUrl,leagueName+ ' League information added failed.',error.message);
-        console.log(error);
+        log.createLog('Football Bot',legueUrl,leagueName+ ' League information updated failed.',error.data.message);
     });
 }
 
