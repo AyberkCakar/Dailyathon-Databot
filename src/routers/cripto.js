0eff23080a1ecac6804b34c4b3cafd4bc87e5a7b
AyberkCakar <ayberkcakar35@gmail.com>
2020-11-15 13:52:59 +0300

    feat: added cripto log service

--- src/routers/cripto.js
+++ src/routers/cripto.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 const mysql = require('mysql');
+const log = require('../service/logService')
 
 var data = '';
-
+var url = 'https://api.collectapi.com/economy/cripto';
 async function getCripto() {
     try {
         const response = await axios({
             method: 'get',
-            url: 'https://api.collectapi.com/economy/cripto',
+            url: url,
             headers: {
                 "content-type": "application/json",
                 "authorization": process.env.API1,
@@ -19,7 +20,7 @@ async function getCripto() {
         )
         await updateCriptoData(data);
     } catch (error) {
-        console.error(error);
+        log.createLog('Cripto Bot',' ', 'getCripto Error.',error.data.message);
     }
 }
 
@@ -33,13 +34,12 @@ async function updateCriptoData(query) {
     await axios.put('https://dailyathon.herokuapp.com/cripto', {
         query: query
     })
-        .then(function (response) {
-            console.log('Cripto');
-            console.log(response.status);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+    .then(function (response) {
+        log.createLog('Cripto Bot',url, ' Cripto information updated successfully.',response.data.message);
+    })
+    .catch(function (error) {
+        log.createLog('Cripto Bot',url, ' Cripto information updated failed.',error.data.message);
+    });
 }
 
 /*
@@ -49,17 +49,16 @@ function insertSql(currency, changeWeek, changeDay, price, code, name) {
     data += sql+"; "
 }
 
-
 async function insertCriptoData(query) {
     await axios.post('https://dailyathon.herokuapp.com/cripto', {
         query:query
     })
-        .then(function (response) {
-            console.log(response.status);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+    .then(function (response) {
+        log.createLog('Cripto Bot',url, ' Cripto information added successfully.',response.data.message);
+    })
+    .catch(function (error) {
+        log.createLog('Cripto Bot',url, ' Cripto information added failed.',error.data.message);
+    })
 }
 */
 
