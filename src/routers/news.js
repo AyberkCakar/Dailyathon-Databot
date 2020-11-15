e3b2c15d70d9ddd7d43f8ab617515bfe93dbadc5
AyberkCakar <ayberkcakar35@gmail.com>
2020-11-15 13:53:17 +0300

    feat: added news log service

--- src/routers/news.js
+++ src/routers/news.js
@@ -1,89 +1,126 @@
 const axios = require('axios');
 const mysql = require('mysql');
+const log = require('../service/logService')
 require('dotenv').config()
 
 var sqlQuery= '';
 
 async function getNews() {
     try {
-        //await businessNews();
-        //await entertainmentNews();
-        //await healthNews();
-        //await sienceNews();
-        //await sportsNews();
-        //await technologyNews();
+        await businessNews();
+        await entertainmentNews();
+        await healthNews();
+        await sienceNews();
+        await sportsNews();
+        await technologyNews();
         await economyNews();
     } catch (error) {
-        console.error(error);
+        log.createLog('News Bot',' ', 'getNews Error.',error.data.message);
     }
 }
 
 async function businessNews() {
-    const path='http://newsapi.org/v2/top-headlines?country=tr&category=business&apiKey='+process.env.NEWAPI;
-    let response = await axios.get(path)
-    response.data.articles.forEach(element => {
-        insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,171)
-    })
-    await insertNewsData(sqlQuery)
+    try {
+        const path= 'http://newsapi.org/v2/top-headlines?country=tr&category=business&apiKey=' +process.env.NEWAPI;
+        let response = await axios.get(path)
+        response.data.articles.forEach(element => {
+            insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,171)
+        })
+        await insertNewsData(sqlQuery)
+        sqlQuery= ' ';
+    } catch (error) {
+        log.createLog('News Bot',path, 'businessNews Error.',error.data.message);
+    }
 }
 
 async function entertainmentNews() {
-    const path='http://newsapi.org/v2/top-headlines?country=tr&category=entertainment&apiKey='+process.env.NEWAPI;
-    let response = await axios.get(path)
-    response.data.articles.forEach(element => {
-        insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,181)
-    })
-    await insertNewsData(sqlQuery)
+    try {
+        const path='http://newsapi.org/v2/top-headlines?country=tr&category=entertainment&apiKey='+process.env.NEWAPI;
+        let response = await axios.get(path)
+        response.data.articles.forEach(element => {
+            insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,181)
+        })
+        await insertNewsData(sqlQuery)
+        sqlQuery= ' ';
+    } catch (error) {
+        log.createLog('News Bot',path, 'entertainmentNews Error.',error.data.message);
+    }
 }
 
 async function healthNews() {
-    const path='http://newsapi.org/v2/top-headlines?country=tr&category=health&apiKey='+process.env.NEWAPI;
-    let response = await axios.get(path)
-    response.data.articles.forEach(element => {
-        insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,191)
-    })
-    await insertNewsData(sqlQuery)
+    try {
+        const path='http://newsapi.org/v2/top-headlines?country=tr&category=health&apiKey='+process.env.NEWAPI;
+        let response = await axios.get(path)
+        response.data.articles.forEach(element => {
+            insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,191)
+        })
+        await insertNewsData(sqlQuery)
+        sqlQuery= ' ';
+    } catch (error) {
+        log.createLog('News Bot',path, 'healthNews Error.',error.data.message);
+    }
 }
 
 async function sienceNews() {
-    const path='http://newsapi.org/v2/top-headlines?country=tr&category=science&apiKey='+process.env.NEWAPI;
-    let response = await axios.get(path)
-    response.data.articles.forEach(element => {
-        insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,201)
-    })
-    await insertNewsData(sqlQuery)
+    try {
+        const path='http://newsapi.org/v2/top-headlines?country=tr&category=science&apiKey='+process.env.NEWAPI;
+        let response = await axios.get(path)
+        response.data.articles.forEach(element => {
+            insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,201)
+        })
+        await insertNewsData(sqlQuery)
+        sqlQuery= ' ';
+    } catch (error) {
+        log.createLog('News Bot',path, 'sienceNews Error.',error.data.message);
+    }
 }
 
 async function sportsNews() {
-    const path='http://newsapi.org/v2/top-headlines?country=tr&category=sports&apiKey='+process.env.NEWAPI;
-    let response = await axios.get(path)
-    response.data.articles.forEach(element => {
-        insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,1)
-    })
-    await insertNewsData(sqlQuery)
+    try {
+        const path='http://newsapi.org/v2/top-headlines?country=tr&category=sports&apiKey='+process.env.NEWAPI;
+        let response = await axios.get(path)
+        response.data.articles.forEach(element => {
+            insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,1)
+        })
+        await insertNewsData(sqlQuery)
+        sqlQuery= ' ';
+    } catch (error) {
+        log.createLog('News Bot',path, 'sportsNews Error.',error.data.message);
+    }
 }
 
 async function technologyNews() {
-    const path='http://newsapi.org/v2/top-headlines?country=tr&category=technology&apiKey='+process.env.NEWAPI;
-    let response = await axios.get(path)
-    response.data.articles.forEach(element => {
-        insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,4)
-    })
-    await insertNewsData(sqlQuery)
+    try {
+        const path='http://newsapi.org/v2/top-headlines?country=tr&category=technology&apiKey='+process.env.NEWAPI;
+        let response = await axios.get(path)
+        response.data.articles.forEach(element => {
+            insertSql(element.title, element.description,element.urlToImage,element.content,element.publishedAt,4)
+        })
+        await insertNewsData(sqlQuery)
+        sqlQuery= ' ';
+    } catch (error) {
+        log.createLog('News Bot',path, 'technologyNews Error.',error.data.message);
+    }
 }
 
 async function economyNews() {
-    const path='https://api.collectapi.com/news/getNews?country=tr&tag=economy';
-    let response = await axios.get(path,{
-        headers: {
-            "content-type": "application/json",
-            "authorization": process.env.API2,
-        }
-    })
-    response.data.result.forEach(element => {
-        insertSql(element.name, element.description,element.image,element.description,Date.now(),3)
-    })
-    await insertNewsData(sqlQuery)
+    try {
+        const path='https://api.collectapi.com/news/getNews?country=tr&tag=economy';
+        let response = await axios.get(path,{
+            headers: {
+                "content-type": "application/json",
+                "authorization": process.env.API2,
+            }
+        })
+        response.data.result.forEach(element => {
+            insertSql(element.name, element.description,element.image,element.description,Date.now(),3)
+        })
+        await insertNewsData(sqlQuery)
+        sqlQuery= ' ';
+    } catch (error) {
+        log.createLog('News Bot',path, 'economyNews Error.',error.data.message);
+    }
+
 }
 
 
@@ -98,12 +135,12 @@ async function insertNewsData(query) {
     await axios.post('https://dailyathon.herokuapp.com/news', {
         query:query
     })
-        .then(function (response) {
-            console.log(response.status);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+    .then(function (response) {
+        log.createLog('News Bot',' ', ' News information added successfully.',response.data.message);
+    })
+    .catch(function (error) {
+        log.createLog('News Bot',' ', ' News information added failed.',error.data.message);
+    })
 }
 
 const hours = (1000 * 60 * 60 * 24);
